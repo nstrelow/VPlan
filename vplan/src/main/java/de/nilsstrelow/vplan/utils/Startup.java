03b65a3d402856842637c195b6ea6142b53f02b1
Nils Strelow <djnilse@gmail.com>
2014-09-25 23:09:13 +0200

    forgot to update version check number

--- vplan/src/main/java/de/nilsstrelow/vplan/utils/Startup.java
+++ vplan/src/main/java/de/nilsstrelow/vplan/utils/Startup.java
@@ -25,7 +25,7 @@ import de.nilsstrelow.vplan.activities.Settings;
 public class Startup implements OnShowcaseEventListener {
 
     public static final String UPDATE_NOTIFICATION = "showed_get_update_notifications";
-    public static final String NEW_VERSION_MSG = "new_version_msg" + "300";
+    public static final String NEW_VERSION_MSG = "new_version_msg" + "310";
     private Activity activity;
     private SharedPreferences sharedPreferences;
     private boolean isTutorialMode = false;
